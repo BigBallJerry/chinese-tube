b348d53aff461edaf66a00f937d149fc9c8af931
Jian <8270876@gmail.com>
2022-02-18 16:03:38 +0100

    Created the nextjs starter for first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+export async function getServerSideProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  
+  return {
+    props: {msg: "Hello World, SSR!"}
+  };
+}
+
+export default function Home() {
+  return <h1 className="m-4 text-center text-4xl text-red-500">Hello world!</h1>
+}
